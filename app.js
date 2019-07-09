dc5b1ded8824916a330aede53ee2859609ae3e6c
Serene Mathew <serenemathew@Serenes-MacBook-Pro.local>
2019-07-10 00:22:09 +0530

    project structure changed to client and server

---
+++ app.js
@@ -0,0 +1,48 @@
+const bodyParser = require('body-parser');
+var createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+var cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const config = require('./conf/server.conf');
+// const router = require('./src/server/routes/index')
+
+var indexRouter = require('./src/server/routes/index');
+var usersRouter = require('./src/server/routes/users');
+
+var app = express();
+
+// port number
+const port = 3000;
+
+// middleware
+app.use(bodyParser.json());
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
